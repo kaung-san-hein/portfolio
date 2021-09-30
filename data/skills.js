06da44de1dee284d7ae0ba737a2c0ff70af4a8d5
kaung-san-hein <kaungsanhein2019@gmail.com>
2021-09-30 19:57:30 +0630

    [Done] data fetching

---
+++ data/skills.js
@@ -0,0 +1,66 @@
+export const skills = [
+  {
+    id: 1,
+    skill: "HTML",
+  },
+  {
+    id: 2,
+    skill: "CSS",
+  },
+  {
+    id: 3,
+    skill: "Javascript",
+  },
+  {
+    id: 4,
+    skill: "PHP",
+  },
+  {
+    id: 5,
+    skill: "Dart",
+  },
+  {
+    id: 6,
+    skill: "Reactjs",
+  },
+  {
+    id: 7,
+    skill: "Nextjs",
+  },
+  {
+    id: 8,
+    skill: "Nodejs",
+  },
+  {
+    id: 9,
+    skill: "Strapi",
+  },
+  {
+    id: 10,
+    skill: "Laravel",
+  },
+  {
+    id: 11,
+    skill: "Flutter",
+  },
+  {
+    id: 12,
+    skill: "MySQL",
+  },
+  {
+    id: 13,
+    skill: "Postgresql",
+  },
+  {
+    id: 14,
+    skill: "MongoDB",
+  },
+  {
+    id: 15,
+    skill: "Sqlite",
+  },
+  {
+    id: 16,
+    skill: "Figma",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,25 @@ import { Fragment } from "react"
 import Contact from "../components/home/Contact"
 import Experience from "../components/home/Experience"
 import Hero from "../components/home/Hero"
+import { projects } from "../data/projects"
+import { skills } from "../data/skills"
 
-export default function Home() {
+export async function getStaticProps() {
+
+  return {
+    props: {
+      projects,
+      skills
+    },
+  }
+}
+
+export default function Home({projects, skills}) {
   return (
     <Fragment>
       <Hero />
       <Contact />
-      <Experience />
+      <Experience projects={projects} skills={skills} />
     </Fragment>
   )
 }
